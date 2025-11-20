d8608b80fa461584db3a23a6dfc17de2b573101d
Rodrigo Bomfim <rodrigo.bomfim2003@gmail.com>
2025-11-20 20:41:59 -0300

    Fix: Fix some errors

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,12 +3,15 @@ import withPWA from 'next-pwa';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
   typescript: {
     ignoreBuildErrors: true,
   },
+  
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  }, 
+
   webpack: (config) => {
     return config;
   },
